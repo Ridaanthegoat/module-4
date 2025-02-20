ba4431941af4ccfc1a46f9349c0cf722935663dc
Ridaanthegoat <ridaan1290@gmail.com>
2025-02-20 19:37:45 +0530

    Add files via upload

---
+++ class 2 activity 1.py
@@ -0,0 +1,8 @@
+def Ridaan(n):
+    iteration=0
+    print("the number entered by user is", n)
+    iteration+=1
+    print("total iterations done on screen is ",iteration,"\n")
+
+Ridaan(10)
+Ridaan(20)
---
+++ class 2 activity 2.py
@@ -0,0 +1,9 @@
+def Ridaan(n):
+    iteration=0
+    for i in range(n,n+10):
+        iteration+=1
+    print("When n is ",n,"iteration is ",iteration)
+
+Ridaan(10)
+Ridaan(20)
+Ridaan(30)
